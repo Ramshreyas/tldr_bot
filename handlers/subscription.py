e527accea4996d56c0b70eb42e5836dbdf15149c
Ramshreyas Rao <ramshreyas.rao@gmail.com>
2024-05-22 13:03:52 +0100

    Fixes sqlalchemy error

--- handlers/subscription.py
+++ handlers/subscription.py
@@ -1,6 +1,7 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 from sqlmodel import Session, select
+from sqlalchemy.orm import Session
 from db.db import get_db
 from db.models import Subscriber
 
@@ -9,10 +10,12 @@ def subscribe_user(update: Update, context: CallbackContext):
     user_id = user.id
     username = user.username
 
-    with next(get_db()) as session:
+    db_session = next(get_db())
+
+    with db_session as session:
         # Check if the user is already subscribed
         statement = select(Subscriber).where(Subscriber.user_id == user_id)
-        existing_subscriber = session.exec(statement).first()
+        existing_subscriber = session.execute(statement).scalars().first()
 
         if existing_subscriber:
             update.message.reply_text("You are already subscribed.")
@@ -28,10 +31,12 @@ def unsubscribe_user(update: Update, context: CallbackContext):
     user = update.effective_user
     user_id = user.id
 
-    with next(get_db()) as session:
+    db_session = next(get_db())
+
+    with db_session as session:
         # Check if the user is subscribed
         statement = select(Subscriber).where(Subscriber.user_id == user_id)
-        existing_subscriber = session.exec(statement).first()
+        existing_subscriber = session.execute(statement).scalars().first()
 
         if existing_subscriber:
             # Remove the user from the Subscriber table
@@ -39,4 +44,4 @@ def unsubscribe_user(update: Update, context: CallbackContext):
             session.commit()
             update.message.reply_text("You have successfully unsubscribed from daily TLDRs.")
         else:
-            update.message.reply_text("You are not subscribed.")
+            update.message.reply_text("You are not subscribed."
