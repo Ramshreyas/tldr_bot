214a10c70361753ba6cd91d5538b2fd24c790f2f
Ramshreyas Rao <ramshreyas.rao@gmail.com>
2024-05-23 10:23:48 +0100

    Switches to async scheduler

--- scheduler/daily_tldr.py
+++ scheduler/daily_tldr.py
@@ -1,25 +1,30 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 from telegram import Bot
 from db.db import get_db
 from db.models import Subscriber
 from tldr.tldr import fetch_latest_tldr, format_tldr
+import logging
+
 
 # Function to send TLDRs to all subscribers
 async def send_daily_tldr(context):
     latest_tldr = fetch_latest_tldr()
     formatted_tldr = format_tldr(latest_tldr)
     
-    with next(get_db()) as session:
+    async with next(get_db()) as session:
         subscribers = session.query(Subscriber).all()
         for subscriber in subscribers:
             try:
                 await context.bot.send_message(chat_id=subscriber.user_id, text=formatted_tldr)
+                logging.info(f"Sent TLDR to {subscriber.user_id}")
             except Exception as e:
-                print(f"Error sending message to {subscriber.user_id}: {e}")
+                logging.error(f"Error sending message to {subscriber.user_id}: {e}")
+
 
 # Setup the scheduler
-def setup_scheduler(bot: Bot):
-    scheduler = BackgroundScheduler(timezone="UTC")
-    scheduler.add_job(send_daily_tldr, CronTrigger(hour=9, minute=15), args=[bot])
+def setup_scheduler(application):
+    scheduler = AsyncIOScheduler(timezone="UTC")
+    scheduler.add_job(send_daily_tldr, CronTrigger(hour=9, minute=25), args=[application])
     scheduler.start()
+    return scheduler
