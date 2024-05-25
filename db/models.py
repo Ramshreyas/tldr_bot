f09ccfc713a5e70836a7826a8a811ead71a46b32
Ramshreyas Rao <ramshreyas.rao@gmail.com>
2024-05-25 18:49:00 +0100

    Makes subscriber user_id a BIGINT

--- db/models.py
+++ db/models.py
@@ -60,5 +60,5 @@ class TLDR(SQLModel, table=True):
 
 class Subscriber(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    user_id: int = Field(index=True)
+    user_id: Optional[int] = Field(default=None, sa_column=Column(BigInteger()))  # Telegram user ID
     username: str
